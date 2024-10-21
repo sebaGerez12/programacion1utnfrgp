4a7b7b8a9c6ac8b294c7e0cbbd4d614ca362502a
sebaGerez12 <sebagerez025@gmail.com>
2024-10-21 13:08:22 -0300

    Add files via upload

---
+++ Guia1 Ej5.cpp
@@ -0,0 +1,45 @@
+// Un comercio vende tres marcas de alfajores distintas A, B y C.
+// Hacer un programa para ingresar por teclado la cantidad de alfajores vendidos de cada una de las tres marcas
+// y luego se informe el porcentaje de ventas para cada una de ellas.
+// Ejemplo. Si se ingresa 100, 25 y 75 como cantidades vendidas entonces el programa calculará e informará A: 50%, B: 12,50% y C: 37,50%.
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+string A, B, C;
+float porcentajeA, porcentajeB, porcentajeC, cien;
+int UnidadesVendidasA, UnidadesVendidasB, UnidadesVendidasC, total;
+
+
+cout << "Ingrese Unidades vendidas marca A:";
+cin >> UnidadesVendidasA;
+cout << endl;
+cout << "Ingrese Unidades vendidas marca B:";
+cin >> UnidadesVendidasB;
+cout << endl;
+cout << "Ingrese Unidades vendidas marca C:";
+cin >> UnidadesVendidasC;
+
+
+total = UnidadesVendidasA + UnidadesVendidasB + UnidadesVendidasC;
+
+porcentajeA = UnidadesVendidasA * 100 / total;
+
+porcentajeB = UnidadesVendidasB * 100 / total;
+
+porcentajeC = UnidadesVendidasC * 100 / total;
+
+
+cout << endl << endl << endl;
+cout << "Porcentaje de ventas de marca A: " << porcentajeA << "%" << endl;
+cout << endl;
+cout << "Porcentaje de ventas de marca B: " << porcentajeB << "%" << endl;
+cout << endl;
+cout << "Porcentaje de ventas de marca C: " << porcentajeC << "%" << endl;
+cout << endl << endl << endl;
+
+	return 0;
+}
---
+++ Guia1 Ej6.cpp
@@ -0,0 +1,42 @@
+//Hacer un programa para que un comercio ingrese por teclado la recaudación en pesos para cada una de las cuatro semanas del mes.
+//El programa debe listar la recaudación promedio por semana y el porcentaje de recaudación por semana.
+//Ejemplo. Si se ingresa $1600, $1200, $4800 y $400 se listará como recaudación promedio $2000 y como porcentajes por semana: 20%, 15%, 60% y 5%
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+float recaudacionSemana1, recaudacionSemana2, recaudacionSemana3, recaudacionSemana4, recaudacionTotal;
+int recaudacionPRO, recaudacionSEM, semanas, cien, RPxS1, RPxS2, RPxS3, RPxS4;
+
+cout << "Ingrese recaudación de Semana 1: ";
+cin >> recaudacionSemana1;
+cout << endl;
+cout << "Ingrese recaudación de Semana 2: ";
+cin >> recaudacionSemana2;
+cout << endl;
+cout << "Ingrese recaudación de Semana 3: ";
+cin >> recaudacionSemana3;
+cout << endl;
+cout << "Ingrese recaudación de Semana 4: ";
+cin >> recaudacionSemana4;
+
+semanas = 4;
+cien = 100;
+recaudacionTotal = recaudacionSemana1 + recaudacionSemana2 + recaudacionSemana3 + recaudacionSemana4;
+recaudacionPRO = recaudacionTotal / semanas;
+RPxS1 = recaudacionSemana1 * 100 / recaudacionTotal;
+RPxS2 = recaudacionSemana1 * 100 / recaudacionTotal;
+RPxS3 = recaudacionSemana1 * 100 / recaudacionTotal;
+RPxS4 = recaudacionSemana1 * 100 / recaudacionTotal;
+
+cout << endl << endl << endl;
+cout << "La recaudacion promedio es de: $" << recaudacionPRO << endl;
+cout << endl;
+cout << "El promedio de recaudacion por cada semana es:" << RPxS1 << "%, "<< RPxS2 << "%, "<< RPxS3 << "%, "<< RPxS4 << "%, " << endl;
+cout << endl << endl;
+
+
+	return 0;
+}
